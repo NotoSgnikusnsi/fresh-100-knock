e6d0ce94e36723907a1da15f0028d6bfa8e6a640
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-10-24 14:48:10 +0900

    style: Timerコンポーネントの基本的なスタイルを作成した

--- 03_timer/islands/Timer.tsx
+++ 03_timer/islands/Timer.tsx
@@ -1,13 +1,28 @@
+import{ useState } from "preact/hooks";
+
 export default function Timer() {
-  return(
-    <div>
-      <h2 class="flex justify-center">Fresh Timer</h2>
-      <h1>00:00:00</h1>
-      <div>
-        <button>スタート</button>
-        <button>ストップ</button>
-        <button>リセット</button>
+  const [start, setStart] = useState(false)
+  return (
+    <div class="rounded shadow-md">
+      <div class="p-4">
+        <h2 class="p-4 flex justify-center text-bold text-xl text-gray-500">
+          Fresh Timer
+        </h2>
+        <h1 class="p-2 flex justify-center text-bold text-4xl">00:00:00</h1>
+        <div class="p-4 flex justify-center items-center">
+          {start
+            ? (
+              <button class="rounded-full border-2 h-12 w-12 bg-white hover:bg-gray-100 text-gray-600" onClick={()=>{setStart(false)}}>
+                ■
+              </button>
+            )
+            : (
+              <button class="rounded-full h-12 w-12 bg-gray-500 hover:bg-gray-600 text-white" onClick={()=>{setStart(true)}}>
+                ▶
+              </button>
+            )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- 03_timer/routes/index.tsx
+++ 03_timer/routes/index.tsx
@@ -1,12 +1,10 @@
 import { useSignal } from "@preact/signals";
-import Timer from "../islands/Counter.tsx";
+import Timer from "../islands/Timer.tsx";
 
 export default function Home() {
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-      <Timer></Timer>
-      </div>
+    <div class="flex items-center justify-center w-screen h-screen">
+      <Timer />
     </div>
   );
 }
