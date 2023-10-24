ba7b1cdcbcf3eaa88712389d04543eee40bfa6a4
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-10-24 12:54:55 +0900

    add: Timerコンポーネントを作成するためのファイルを作成した

---
+++ 03_timer/islands/Timer.tsx
@@ -0,0 +1,13 @@
+export default function Timer() {
+  return(
+    <div>
+      <h2 class="flex justify-center">Fresh Timer</h2>
+      <h1>00:00:00</h1>
+      <div>
+        <button>スタート</button>
+        <button>ストップ</button>
+        <button>リセット</button>
+      </div>
+    </div>
+  )
+}
